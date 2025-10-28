909daaf8660df535a9e6173e745b10e682e8863f
Shehryar <shehryar@flicken.io>
2025-10-28 15:43:16 +0500

    Add Search Functionality

--- safer_dotus/urls.py
+++ safer_dotus/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('listings.urls')),  # 👈 add this
+    path('', include('safer_app_listing.urls')),  # 👈 add this
 ]
