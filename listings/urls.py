b11417f88bda79d64006c13d20057611642b7c1d
Shehryar <shehryar@flicken.io>
2025-10-29 17:01:10 +0500

    Add download CSV functionality

--- listings/urls.py
+++ listings/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('safer/', views.safer_data_view, name='safer_data'),
+    path('download_csv/', views.download_csv, name='download_csv'),
 ]
---
+++ safer_app_listing/views.py
@@ -0,0 +1,81 @@
+import csv
+
+from .models import SaferData
+from django.db.models import Q
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.paginator import Paginator
+
+
+def build_filtered_queryset(request, base_queryset=None):
+    if base_queryset is None:
+        base_queryset = SaferData.objects.all()
+
+    query = request.GET.get('q', '').strip()
+    dot_from = request.GET.get('dot_from')
+    dot_to = request.GET.get('dot_to')
+
+    if query:
+        filters = Q()
+
+        # Numeric queries
+        if query.isdigit():
+            filters |= Q(id=int(query)) | Q(dot_number=int(query)) | Q(zipcode=int(query)) | Q(phone=int(query))
+        else:
+            # Text-based partial matches
+            filters |= (
+                    Q(legal_name__icontains=query)
+                    | Q(physical_address__icontains=query)
+                    | Q(mailing_code__icontains=query)
+                    | Q(operating_status__icontains=query)
+            )
+
+        base_queryset = base_queryset.filter(filters)
+
+    # DOT number range filters
+    if dot_from and dot_to:
+        base_queryset = base_queryset.filter(dot_number__range=[dot_from, dot_to])
+    elif dot_from:
+        base_queryset = base_queryset.filter(dot_number__gte=dot_from)
+    elif dot_to:
+        base_queryset = base_queryset.filter(dot_number__lte=dot_to)
+
+    return base_queryset
+
+
+def safer_data_view(request):
+    data = build_filtered_queryset(request)
+    paginator = Paginator(data, 50)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'data': page_obj,
+        'query': request.GET.get('q', '').strip(),
+        'dot_from': request.GET.get('dot_from'),
+        'dot_to': request.GET.get('dot_to'),
+    }
+    return render(request, 'safer_app_listing/safer_data.html', context)
+
+
+def download_csv(request):
+    safer_data = build_filtered_queryset(request)
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="safer_data.csv"'
+    writer = csv.writer(response)
+
+    writer.writerow([
+        'id', 'dot_number', 'legal_name', 'physical_address', 'zipcode',
+        'mailing_code', 'phone', 'operating_status', 'power_units',
+        'drivers', 'date_filed', 'fetched_at'
+    ])
+
+    for data in safer_data:
+        writer.writerow([
+            data.id, data.dot_number, data.legal_name, data.physical_address,
+            data.zipcode, data.mailing_code, data.phone, data.operating_status,
+            data.power_units, data.drivers, data.date_filed, data.fetched_at
+        ])
+
+    return response
